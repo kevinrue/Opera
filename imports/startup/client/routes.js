05fde00cc25a3ae7b48763fc9d7d03c6074d1b4c
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2017-02-28 23:20:31 +0000

    added NavBar persistent across pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
 
 // route components
 import App from '../../ui/App.jsx';
@@ -11,9 +11,27 @@ import App from '../../ui/App.jsx';
 const NotFound = () => (
   <h1>404.. This page is not found!</h1>)
 
+const NotExist = () => (
+  <h1>This is the "NotExist" page!</h1>)
+
+const Nav = () => (
+  <div>
+    <Link to='/'>Home</Link>&nbsp;
+    <Link to='/404'>404</Link>
+  </div>
+)
+
+const Container = (props) => <div>
+  <Nav />
+  {props.children}
+</div>
+
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    <Route exact path="/" component={App} />
-    <Route path='*' component={NotFound} />
+  	<Route path='/' component={Container}>
+  		<IndexRoute component={App} />
+  		<Route path='404' component={NotFound} />
+  		<Route path='*' component={NotFound} />
+  	</Route>    
   </Router>
 );
