ab63586819e5b5f372d68bdb77fb71ac2dffa4c5
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2017-03-09 22:13:40 +0000

    Upgraded Sequencers to React BootstrapTable

--- imports/ui/SequencersTable.jsx
+++ imports/ui/SequencersTable.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
 import Loading from './loading.jsx'
 
 import { Sequencers } from '../api/sequencers.js';
@@ -17,36 +19,51 @@ class SequencersTable extends Component {
 
 	renderTable () {
 		return(
-			<table className='table sequencers-table'>
-      	<thead>
-      		<tr>
-      			<th className='sequencer-field-label'>Label</th>
-      			<th className='ssequencer-field-value'>Value in database</th>
-      		</tr>
-      	</thead>
-      	<tbody>
-      		{this.props.sequencers.map((sequencer) => (
-      			<tr key={sequencer._id}>
-      				<td>{sequencer.label}</td>
-      				<td>{sequencer.value}</td>
-      			</tr>
-      		))}
-      	</tbody>
-      </table>
+			<BootstrapTable data={this.props.sequencers} striped={true} hover={true} keyField='_id'>
+        <TableHeaderColumn
+        	dataField="label"
+        	dataAlign="center"
+        	width="60%"
+        	dataSort={true}>
+        	Label
+        </TableHeaderColumn>
+        <TableHeaderColumn
+	        dataField="value"
+	        dataAlign="center"
+	        width="40%">
+          Database value
+        </TableHeaderColumn>
+      </BootstrapTable>
     );
 	}
 
+	renderMainPanel() {
+    return(
+      <div className='main-panel'>
+        <header><h1>Sequencers</h1></header>
+        { this.props.loading ? <Loading /> : this.renderTable() }
+      </div>
+    );
+  }
 
-	render () {
-		return(
-			<div>
-				<header><h1>Sequencers</h1></header>
+	renderAdminPanel() {
+    return(
+      <div className='admin-panel'>
+      </div>
+    );
+  }
 
-				{ this.props.loading ? <Loading /> : this.renderTable() }
-				
-			</div>
-		);
-	}
+	render() {
+    return (
+    	<div>
+        { this.renderMainPanel() }
+
+        { this.props.currentUser ? this.renderAdminPanel() : '' }
+
+        <div id="clearingdiv"></div>
+    	</div>
+    );
+  }
 
 }
 
