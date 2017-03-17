6e94e1a410ad690702ce4248c0c5ba415fd75f18
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2017-03-17 20:03:32 +0000

    fix: dummy test

--- imports/api/experiments/experiments.tests.js
+++ imports/api/experiments/experiments.tests.js
@@ -14,25 +14,23 @@ if (Meteor.isServer) {
 
 		beforeEach(() => {
 			Experiments.remove({});
-			experimentId = Experiments.insert({
-			  name: 'test experiment',
-			  Nsamples: 0,
-			});
+			
 		});
 
-      it('can delete experiments', () => {
+      it('can create an experiment', () => {
       	// Find the internal implementation of the experiment method so we can
         // test it in isolation
-        const deleteExperiment = Meteor.server.method_handlers['experiments.remove'];
+        const addExperiment = Meteor.server.method_handlers['experiments.insert'];
  
         // Set up a fake method invocation that looks like what the method expects
         const invocation = { userId }; // Note: short for {userId: userId}
+        const newName = 'test experiment';
  
         // Run the method with `this` set to the fake invocation
-        deleteExperiment.apply(invocation, [experimentId]);
+        addExperiment.apply(invocation, [newName]);
  
         // Verify that the method does what we expected
-        assert.equal(Experiments.find().count(), 0);
+        assert.equal(Experiments.find().count(), 1);
       });
     });
   });
